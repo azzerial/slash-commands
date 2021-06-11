204800761465758768f8fc50f36bdd2d755fd2d1
Azzerial <the.azzerial@icloud.com>
2021-06-11 02:31:32 +0100

    added: SlashCommand class

---
+++ api/src/main/java/com/github/azzerial/slash/SlashCommand.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2021 Robin Mercier
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.azzerial.slash;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.interactions.commands.build.CommandData;
+
+public final class SlashCommand {
+
+    private final JDA jda;
+    private final String tag;
+    private final CommandData data;
+
+    /* Constructors */
+
+    public SlashCommand(JDA jda, String tag, CommandData data) {
+        this.jda = jda;
+        this.tag = tag;
+        this.data = data;
+    }
+
+    /* Getters & Setters */
+
+    public String getTag() {
+        return tag;
+    }
+}
