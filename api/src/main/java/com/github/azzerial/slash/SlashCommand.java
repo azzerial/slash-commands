96b2dc81ba6e23ed6feaab368fc9a0836b37f466
Azzerial <the.azzerial@icloud.com>
2021-06-11 02:52:16 +0100

    updated: implemented JDA command interactions in SlashCommand class

--- api/src/main/java/com/github/azzerial/slash/SlashCommand.java
+++ api/src/main/java/com/github/azzerial/slash/SlashCommand.java
@@ -17,13 +17,27 @@
 package com.github.azzerial.slash;
 
 import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.interactions.commands.Command;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
+import net.dv8tion.jda.api.interactions.commands.privileges.CommandPrivilege;
+import net.dv8tion.jda.api.requests.RestAction;
+import net.dv8tion.jda.internal.utils.Checks;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicReference;
 
 public final class SlashCommand {
+
+    private static final long GLOBAL = -1L;
 
     private final JDA jda;
     private final String tag;
     private final CommandData data;
+    private final Map<Long, AtomicReference<Command>> instances = new HashMap<>();
 
     /* Constructors */
 
@@ -37,5 +51,151 @@ public final class SlashCommand {
 
     public String getTag() {
         return tag;
+    }
+
+    /* Methods */
+
+    public synchronized SlashCommand deleteGlobal() {
+        if (instances.containsKey(GLOBAL)) {
+            final AtomicReference<Command> command = instances.get(GLOBAL);
+
+            command.get().delete().queue();
+            instances.remove(GLOBAL);
+        }
+        return this;
+    }
+
+    public SlashCommand deleteGuild(long id) {
+        return deleteGuild(jda.getGuildById(id));
+    }
+
+    public SlashCommand deleteGuild(String id) {
+        return deleteGuild(jda.getGuildById(id));
+    }
+
+    public synchronized SlashCommand deleteGuild(Guild guild) {
+        Checks.notNull(guild, "Guild");
+        if (instances.containsKey(guild.getIdLong())) {
+            final AtomicReference<Command> command = instances.get(guild.getIdLong());
+
+            command.get().delete().queue();
+            instances.remove(guild.getIdLong());
+        }
+        return this;
+    }
+
+    public RestAction<List<CommandPrivilege>> retrieveGlobalPrivileges(long id) {
+        return retrieveGlobalPrivileges(jda.getGuildById(id));
+    }
+
+    public RestAction<List<CommandPrivilege>> retrieveGlobalPrivileges(String id) {
+        return retrieveGlobalPrivileges(jda.getGuildById(id));
+    }
+
+    public synchronized RestAction<List<CommandPrivilege>> retrieveGlobalPrivileges(Guild guild) {
+        Checks.notNull(guild, "Guild");
+        return instances.containsKey(GLOBAL) ?
+            instances.get(GLOBAL).get().retrievePrivileges(guild) :
+            null;
+    }
+
+    public RestAction<List<CommandPrivilege>> retrieveGuildPrivileges(long id) {
+        return retrieveGuildPrivileges(jda.getGuildById(id));
+    }
+
+    public RestAction<List<CommandPrivilege>> retrieveGuildPrivileges(String id) {
+        return retrieveGuildPrivileges(jda.getGuildById(id));
+    }
+
+    public synchronized RestAction<List<CommandPrivilege>> retrieveGuildPrivileges(Guild guild) {
+        Checks.notNull(guild, "Guild");
+        return instances.containsKey(guild.getIdLong()) ?
+            instances.get(guild.getIdLong()).get().retrievePrivileges(guild) :
+            null;
+    }
+
+    public RestAction<List<CommandPrivilege>> updateGlobalPrivileges(long id, CommandPrivilege... privileges) {
+        return updateGlobalPrivileges(jda.getGuildById(id), privileges);
+    }
+
+    public RestAction<List<CommandPrivilege>> updateGlobalPrivileges(String id, CommandPrivilege... privileges) {
+        return updateGlobalPrivileges(jda.getGuildById(id), privileges);
+    }
+
+    public synchronized RestAction<List<CommandPrivilege>> updateGlobalPrivileges(Guild guild, CommandPrivilege... privileges) {
+        Checks.notNull(guild, "Guild");
+        Checks.noneNull(privileges, "CommandPrivileges");
+        return instances.containsKey(GLOBAL) ?
+            instances.get(GLOBAL).get().updatePrivileges(guild, privileges) :
+            null;
+    }
+
+    public RestAction<List<CommandPrivilege>> updateGlobalPrivileges(long id, Collection<CommandPrivilege> privileges) {
+        return updateGlobalPrivileges(jda.getGuildById(id), privileges);
+    }
+
+    public RestAction<List<CommandPrivilege>> updateGlobalPrivileges(String id, Collection<CommandPrivilege> privileges) {
+        return updateGlobalPrivileges(jda.getGuildById(id), privileges);
+    }
+
+    public synchronized RestAction<List<CommandPrivilege>> updateGlobalPrivileges(Guild guild, Collection<CommandPrivilege> privileges) {
+        Checks.notNull(guild, "Guild");
+        Checks.noneNull(privileges, "CommandPrivileges");
+        return instances.containsKey(GLOBAL) ?
+            instances.get(GLOBAL).get().updatePrivileges(guild, privileges) :
+            null;
+    }
+
+    public RestAction<List<CommandPrivilege>> updateGuildPrivileges(long id, CommandPrivilege... privileges) {
+        return updateGuildPrivileges(jda.getGuildById(id), privileges);
+    }
+
+    public RestAction<List<CommandPrivilege>> updateGuildPrivileges(String id, CommandPrivilege... privileges) {
+        return updateGuildPrivileges(jda.getGuildById(id), privileges);
+    }
+
+    public synchronized RestAction<List<CommandPrivilege>> updateGuildPrivileges(Guild guild, CommandPrivilege... privileges) {
+        Checks.notNull(guild, "Guild");
+        Checks.noneNull(privileges, "CommandPrivileges");
+        return instances.containsKey(guild.getIdLong()) ?
+            instances.get(guild.getIdLong()).get().updatePrivileges(guild, privileges) :
+            null;
+    }
+
+    public RestAction<List<CommandPrivilege>> updateGuildPrivileges(long id, Collection<CommandPrivilege> privileges) {
+        return updateGuildPrivileges(jda.getGuildById(id), privileges);
+    }
+
+    public RestAction<List<CommandPrivilege>> updateGuildPrivileges(String id, Collection<CommandPrivilege> privileges) {
+        return updateGuildPrivileges(jda.getGuildById(id), privileges);
+    }
+
+    public synchronized RestAction<List<CommandPrivilege>> updateGuildPrivileges(Guild guild, Collection<CommandPrivilege> privileges) {
+        Checks.notNull(guild, "Guild");
+        Checks.noneNull(privileges, "CommandPrivileges");
+        return instances.containsKey(guild.getIdLong()) ?
+            instances.get(guild.getIdLong()).get().updatePrivileges(guild, privileges) :
+            null;
+    }
+
+    public synchronized SlashCommand upsertGlobal() {
+        jda.upsertCommand(data)
+            .queue(command -> instances.put(GLOBAL, new AtomicReference<>(command)));
+        return this;
+    }
+
+    public SlashCommand upsertGuild(long id) {
+        return upsertGuild(jda.getGuildById(id));
+    }
+
+    public SlashCommand upsertGuild(String id) {
+        return upsertGuild(jda.getGuildById(id));
+    }
+
+    public synchronized SlashCommand upsertGuild(Guild guild) {
+        Checks.notNull(guild, "Guild");
+        guild.upsertCommand(data)
+            .queue(command -> instances.put(guild.getIdLong(), new AtomicReference<>(command)));
+        return this;
     }
 }
