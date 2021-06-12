6a2550dab72e82e84c6ecb1b8117e9c414d71ba3
Azzerial <the.azzerial@icloud.com>
2021-06-12 04:32:09 +0100

    feature: added unique session stores and session timeouts

--- api/src/main/java/com/github/azzerial/slash/util/Session.java
+++ api/src/main/java/com/github/azzerial/slash/util/Session.java
@@ -16,7 +16,6 @@
 
 package com.github.azzerial.slash.util;
 
-import com.github.azzerial.slash.internal.ButtonRegistry;
 import net.dv8tion.jda.api.interactions.InteractionHook;
 import net.dv8tion.jda.api.utils.data.DataObject;
 import net.dv8tion.jda.internal.utils.Checks;
@@ -28,10 +27,16 @@ import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
 
+import static com.github.azzerial.slash.internal.ButtonRegistry.CODE_LENGTH;
+
 public final class Session extends DataObject {
+
+    public static final long DEFAULT_TIMEOUT = 60_000L;
+    public static final TimeUnit DEFAULT_TIMEOUT_UNIT = TimeUnit.MILLISECONDS;
 
     private static final String UUID_REGEX = "[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}";
 
@@ -39,14 +44,31 @@ public final class Session extends DataObject {
     private static final Map<UUID, Session> sessions = new HashMap<>();
 
     private final UUID uuid;
+    private final Map<UUID, DataObject> storage = new HashMap<>();
 
-    private boolean isTimeoutSet;
+    private final long timeout;
+    private final TimeUnit unit;
+    private final InteractionHook hook;
+    private final Consumer<InteractionHook> action;
+    private ScheduledFuture<?> thread;
 
     /* Constructors */
 
-    private Session(UUID uuid) {
-        super(new HashMap<>());
+    private Session(Session session, Map<String, Object> data) {
+        this(session.uuid, data, session.timeout, session.unit, session.hook, session.action);
+    }
+
+    private Session(UUID uuid, Map<String, Object> data, long timeout, TimeUnit unit, InteractionHook hook, Consumer<InteractionHook> action) {
+        super(data);
+        Checks.notNull(uuid, "UUID");
+        Checks.notNegative(timeout, "Timeout");
         this.uuid = uuid;
+        this.timeout = timeout;
+        this.unit = unit;
+        this.hook = hook;
+        this.action = action;
+
+        startTimeoutThread();
     }
 
     /* Getters & Setters */
@@ -58,39 +80,30 @@ public final class Session extends DataObject {
     /* Methods */
 
     public static Session create() {
-        final UUID uuid = UUID.randomUUID();
-        final Session session = new Session(uuid);
+        return create(DEFAULT_TIMEOUT, DEFAULT_TIMEOUT_UNIT, null, null);
+    }
 
+    public static Session create(long timeout, TimeUnit unit) {
+        return create(timeout, unit, null, null);
+    }
+
+    public static Session create(long timeout, TimeUnit unit, InteractionHook hook, Consumer<InteractionHook> action) {
+        Checks.positive(timeout, "Timeout");
+        Checks.notNull(unit, "Unit");
+        final UUID uuid = UUID.randomUUID();
+        final Session session = new Session(uuid, new HashMap<>(), timeout, unit, hook, action);
+
+        System.out.println("created: " + uuid);
         sessions.put(uuid, session);
         return session;
     }
 
     public static Session load(String id) {
-        Checks.notNull(id, "Id");
-        if (id.length() != ButtonRegistry.CODE_LENGTH + 36 && !id.matches(UUID_REGEX)) {
-            throw new IllegalArgumentException("The id is invalid!");
-        }
-        final UUID uuid = UUID.fromString(id.substring(ButtonRegistry.CODE_LENGTH, ButtonRegistry.CODE_LENGTH + 36));
-        return sessions.remove(uuid);
+        return get(id, false);
     }
 
-    public Session setTimeout(long timeout, TimeUnit unit) {
-        return setTimeout(timeout, unit, null, null);
-    }
-
-    public Session setTimeout(long timeout, TimeUnit unit, InteractionHook hook, Consumer<InteractionHook> action) {
-        if (isTimeoutSet) {
-            throw new IllegalStateException("The timeout has already been set!");
-        }
-        Checks.positive(timeout, "Timeout");
-        Checks.notNull(unit, "Unit");
-        this.isTimeoutSet = true;
-        threadpool.schedule(() -> {
-            if (sessions.remove(uuid) != null && hook != null && action != null) {
-                action.accept(hook);
-            }
-        }, timeout, unit);
-        return this;
+    public static Session renew(String id) {
+        return get(id, true);
     }
 
     @NotNull
@@ -114,10 +127,70 @@ public final class Session extends DataObject {
         return this;
     }
 
-    @NotNull
-    @Override
-    public Session toData() {
-        super.toData();
-        return this;
+    public String store(Consumer<DataObject> consumer) {
+        final UUID uuid = UUID.randomUUID();
+        final DataObject data = DataObject.fromJson(toJson());
+
+        consumer.accept(data);
+        storage.put(uuid, data);
+        return getUuid() + uuid;
+    }
+
+    /* Internal */
+
+    private static Session get(String id, boolean renew) {
+        Checks.notNull(id, "Id");
+        if (id.length() != CODE_LENGTH + 36 && id.length() != CODE_LENGTH + 72) {
+            throw new IllegalArgumentException("The id is invalid!");
+        }
+
+        final String sessionStr = id.substring(CODE_LENGTH, CODE_LENGTH + 36);
+        final String storageStr = id.length() == CODE_LENGTH + 72 ?
+            id.substring(CODE_LENGTH  +36, CODE_LENGTH + 72) :
+            null;
+
+        if (!sessionStr.matches(UUID_REGEX) || (storageStr != null && !storageStr.matches(UUID_REGEX))) {
+            throw new IllegalArgumentException("The id is invalid!");
+        }
+
+        final UUID sessionUuid = UUID.fromString(sessionStr);
+        Session session = sessions.remove(sessionUuid);
+
+        if (session == null) {
+            return null;
+        } else if (session.thread != null && !session.thread.isDone()) {
+            session.thread.cancel(true);
+        }
+
+        final UUID storageUuid = storageStr != null ? UUID.fromString(storageStr) : null;
+        final DataObject data = session.storage.get(storageUuid);
+
+        if (data != null && !data.keys().isEmpty()) {
+            session = new Session(session, data.toMap());
+        }
+        if (renew) {
+            session.startTimeoutThread();
+            System.out.println("renewed: " + session.uuid);
+        } else {
+            System.out.println("loaded: " + session.uuid);
+        }
+        return session;
+    }
+
+    private void startTimeoutThread() {
+        if (timeout > 0 && unit != null) {
+            if (thread != null && !thread.isDone()) {
+                return;
+            }
+
+            this.thread = threadpool.schedule(() -> {
+                System.out.println("destroyed: " + uuid);
+                if (sessions.remove(uuid) != null && hook != null && action != null) {
+                    action.accept(hook);
+                }
+            }, timeout, unit);
+        } else {
+            this.thread = null;
+        }
     }
 }
